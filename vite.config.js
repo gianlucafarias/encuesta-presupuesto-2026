da3ffb83b62d007a15041e6329af80a66dc8997e
gianlucafarias <palmiergianluca@gmail.com>
2025-08-01 10:38:55 -0300

    fix vite conf

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 4321,
-    host: true,
+    host: '0.0.0.0',
     allowedHosts: ['encuesta.ceres.gob.ar', 'localhost', '127.0.0.1', '66.97.47.243']
   },
   preview: {
     port: 4321,
-    host: true,
+    host: '0.0.0.0',
     allowedHosts: ['encuesta.ceres.gob.ar', 'localhost', '127.0.0.1', '66.97.47.243']
   }
 });
